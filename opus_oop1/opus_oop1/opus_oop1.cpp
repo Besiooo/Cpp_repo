ca3694d9bba1dbd8cc126f6d7dd8c2fc9b326d45
Besiooo <91867829+Besiooo@users.noreply.github.com>
2022-03-21 15:28:03 +0100

    Add files via upload

---
+++ opus_oop1/opus_oop1/opus_oop1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Flower {
+public:
+    int sprouting_time;
+    int blooming_time;
+
+    string name;
+
+    void Sprout() {
+        cout << name <<" is sprouting!";
+    }
+
+    void Bloom() {
+        cout << name << " is blooming!";
+    }
+};
+
+int main()
+{
+    Flower sunflower;
+    sunflower.name = "Sunflower";
+
+    cout << "How long does " << sunflower.name << " sprout? -> ";
+    cin >> sunflower.sprouting_time;
+
+    cout << "How long does " << sunflower.name << " bloom? -> ";
+    cin >> sunflower.blooming_time;
+
+    cout << sunflower.name << ", sprouting time of " << sunflower.sprouting_time << " and blooming time of " << sunflower.blooming_time << " has been created!";
+
+}
