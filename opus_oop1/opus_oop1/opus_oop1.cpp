d26ed91a4c025f260711747c9b9cf444eba8588e
Besiooo <91867829+Besiooo@users.noreply.github.com>
2022-03-26 10:57:02 +0100

    Update opus_oop1.cpp

--- opus_oop1/opus_oop1/opus_oop1.cpp
+++ opus_oop1/opus_oop1/opus_oop1.cpp
@@ -1,7 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <windows.h>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -23,7 +24,7 @@ public:
     }
 
     void Sprout() {
-        cout << name <<" is sprouting!\n";
+        cout << name << " is sprouting!\n";
         hasSprouted = true;
     }
 
@@ -40,19 +41,7 @@ public:
 
 vector<Flower*> flowerbase;
 
-void addFlower() {
-    int s, b;
-    string flower;
-    
-    cout << "What flower do you want to add? -> ";
-    cin >> flower;
-
-    cout << "How long does " << flower << " sprout? -> ";
-    cin >> s;
-
-    cout << "How long does " << flower << " bloom? -> ";
-    cin >> b;
-
+void addFlower(string flower, int s, int b) {
     Flower* obj = new Flower(flower, s, b);
     flowerbase.push_back(obj);
 }
@@ -66,21 +55,95 @@ void grow() {
                 (*flowerbase[j]).Sprout();
             if (i == (*flowerbase[j]).blooming_time)
                 (*flowerbase[j]).Bloom();
-        }       
+        }
+    }
+}
+
+void loadFlowers() {
+    vector<string> names = { "Violet", "Sunflower", "Rose", "Artichoke", "Lilac" };
+    int rs, rb;
+    for (int i = 0; i < names.size(); i++) {
+        do {
+            rs = rand() % 59 + 1;
+            rb = rand() % 59 + 1;
+        } while (rb <= rs);
+
+        addFlower(names[i], rs, rb);
+    }
+}
+
+void myFlowers() {
+    cout << "How many flowers do you want to add? -> ";
+    int n;
+    cin >> n;
+
+    int s, b;
+    string flower;
+
+    for (int i = 0; i < n; i++) {
+        cout << "What flower do you want to add? -> ";
+        cin >> flower;
+
+        cout << "How long does " << flower << " sprout? -> ";
+        cin >> s;
+
+        cout << "How long does " << flower << " bloom? -> ";
+        cin >> b;
+        addFlower(flower, s, b);
+    }
+}
+
+void loadMatrix() {
+    cout << "T\t";
+
+    for (int i = 0; i < flowerbase.size(); i++) {
+        cout << (*flowerbase[i]).name[0] << "\t";
+    }
+    cout << "\n";
+
+    for (int i = 1; i < 60; i++) {
+        Sleep(1000);
+        cout << i << "\t";
+        for (int j = 0; j < flowerbase.size(); j++) {
+            if (i == (*flowerbase[j]).sprouting_time){
+                cout << "S\t";
+                (*flowerbase[j]).hasSprouted = true;
+            }
+            if (i == (*flowerbase[j]).blooming_time){
+                cout << "B\t";
+                (*flowerbase[j]).hasBloomed = true;
+            }
+            if (i > (*flowerbase[j]).sprouting_time && i < (*flowerbase[j]).blooming_time)
+                cout << "|\t";
+            else if (i != (*flowerbase[j]).sprouting_time && i != (*flowerbase[j]).blooming_time)
+                cout << ".\t";
+        }
+        cout << "\n";
     }
 }
 
 int main()
 {
-    cout << "How many flowers do you want to add? -> ";
-    int n;
-    cin >> n;
+    srand(time(NULL));
 
-    for (int i = 0; i < n; i++) {
-        addFlower();
+    cout << "Do you want to precache floweras or make your own?\n1: create your own\n2: precache\n\t-> ";
+    char decision;
+    cin >> decision;
+
+    switch (decision) {
+    case '1': myFlowers(); break;
+    case '2': loadFlowers(); break;
+    default: break;
     }
 
-    grow();
+    cout << "Matrix mode or text mode?\n1: matrix mode\n2: text mode\n\t-> ";
+    cin >> decision;
+
+    switch (decision) {
+        case '1': loadMatrix(); break;
+        case '2': grow(); break;
+        default: break;
+    }
 
     return 0;
 }
